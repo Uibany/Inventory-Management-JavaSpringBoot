856b58e57b1502a25eae1f1eaa6e447815c5bdf3
Sean Ooi <69132873+seano188@users.noreply.github.com>
2020-12-10 14:47:41 +0800

    associations for usage details

--- team8ca/src/main/java/sg/edu/iss/team8ca/model/Inventory.java
+++ team8ca/src/main/java/sg/edu/iss/team8ca/model/Inventory.java
@@ -1,5 +1,15 @@
 package sg.edu.iss.team8ca.model;
 
+import java.util.List;
+
+import javax.persistence.OneToMany;
+
 public class Inventory {
+	
+	
+	
+	
+	@OneToMany (mappedBy = "inventory")
+	private List<UsageDetails> usagedetails;
 
 }
--- team8ca/src/main/java/sg/edu/iss/team8ca/model/Usage.java
+++ team8ca/src/main/java/sg/edu/iss/team8ca/model/Usage.java
@@ -1,5 +1,22 @@
 package sg.edu.iss.team8ca.model;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 public class Usage {
 
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private long id;
+	private Date usageDate; 
+	
+	@OneToMany(mappedBy = "usage")
+	private List<UsageDetails> usageDetails;
+	
+	
 }
