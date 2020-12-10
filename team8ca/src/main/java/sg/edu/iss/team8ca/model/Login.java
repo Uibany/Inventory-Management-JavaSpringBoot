b2ff346f889f4d8366b761f1962ddef6b4eb5b45
Sankalp <sink257@hotmail.com>
2020-12-10 17:06:46 +0800

    Added login models

---
+++ team8ca/src/main/java/sg/edu/iss/team8ca/model/Login.java
@@ -0,0 +1,9 @@
+package sg.edu.iss.team8ca.model;
+
+public class Login {
+
+	public Login() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ team8ca/src/main/java/sg/edu/iss/team8ca/service/LoginServiceImpl.java
@@ -0,0 +1,9 @@
+package sg.edu.iss.team8ca.service;
+
+public class LoginServiceImpl {
+
+	public LoginServiceImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
