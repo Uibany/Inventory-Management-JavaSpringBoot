8d0d4050742c94f3e78e16b7552a3403ed5ef0f4
Sankalp <sink257@hotmail.com>
2020-12-11 00:36:13 +0800

    Login implementation

--- team8ca/src/main/java/sg/edu/iss/team8ca/model/Login.java
+++ team8ca/src/main/java/sg/edu/iss/team8ca/model/Login.java
@@ -1,28 +1,44 @@
 package sg.edu.iss.team8ca.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Login {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String username;
-	private String passowrd;
+	private String password;
+	private UserType user;
 	
 	public Login() {
 		super();
 	}
 
-	public Login(int id, String username, String passowrd) {
+	public Login(int id, String username, String password, UserType user) {
 		super();
 		this.id = id;
 		this.username = username;
-		this.passowrd = passowrd;
+		this.password = password;
+		this.user = user;
 	}
 
-	public Login(String username, String passowrd) {
+	public Login(String username, String password, UserType user) {
 		super();
 		this.username = username;
-		this.passowrd = passowrd;
+		this.password = password;
+		this.user = user;
 	}
 
+	public Login(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
 
 	public int getId() {
 		return id;
@@ -40,12 +56,20 @@ public class Login {
 		this.username = username;
 	}
 
-	public String getPassowrd() {
-		return passowrd;
+	public String getPassword() {
+		return password;
 	}
 
-	public void setPassowrd(String passowrd) {
-		this.passowrd = passowrd;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
+	public UserType getUser() {
+		return user;
+	}
+
+	public void setUser(UserType user) {
+		this.user = user;
+	}
+	
 }
--- team8ca/src/main/java/sg/edu/iss/team8ca/repo/LoginRepo.java
+++ team8ca/src/main/java/sg/edu/iss/team8ca/repo/LoginRepo.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import sg.edu.iss.team8ca.model.Login;
 
-public interface LoginRepo extends JpaRepository<Login, String> {
+public interface LoginRepo extends JpaRepository<Login, Long> {
+	
+	public Login findLoginByUserName(String u);
 	
 }
