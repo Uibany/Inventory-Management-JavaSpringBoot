f89ae6780509029dac5ed3760d6a0b222d0f10cb
Sean Ooi <69132873+seano188@users.noreply.github.com>
2020-12-10 12:10:40 +0800

    Initial commit

---
+++ team8ca/src/main/java/sg/edu/iss/team8ca/Team8caApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.team8ca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team8caApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team8caApplication.class, args);
+	}
+
+}
