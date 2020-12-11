30e0da1afca09d613821e1ec8063580a0f28744c
Sean Ooi <69132873+seano188@users.noreply.github.com>
2020-12-11 13:13:25 +0800

    hmm

--- team8ca/src/main/java/sg/edu/iss/team8ca/service/InvUsageInterface.java
+++ team8ca/src/main/java/sg/edu/iss/team8ca/service/InvUsageInterface.java
@@ -3,7 +3,6 @@ package sg.edu.iss.team8ca.service;
 import java.util.Date;
 import java.util.List;
 
-
 public interface InvUsageInterface {
 	public void addInvUsage(InvUsageInterface invUsage);
 	public List<InvUsageInterface> listInvUsageById(Integer id);
--- team8ca/src/main/java/sg/edu/iss/team8ca/service/UsageFormInterface.java
+++
@@ -1,7 +0,0 @@
-package sg.edu.iss.team8ca.service;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface UsageFormInterface{
-
-}
