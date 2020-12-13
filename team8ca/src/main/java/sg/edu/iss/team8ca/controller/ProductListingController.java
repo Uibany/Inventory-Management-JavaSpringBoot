2d7724a4f03f1868ea9b7ac5fc216d60d7ad0aa0
frozzon <31442478+frozzon@users.noreply.github.com>
2020-12-13 19:11:07 +0800

    minor changes, added starters?

---
+++ team8ca/src/main/java/sg/edu/iss/team8ca/controller/ProductListingController.java
@@ -0,0 +1,11 @@
+package sg.edu.iss.team8ca.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ProductListing")
+
+public class ProductListingController {
+
+}
