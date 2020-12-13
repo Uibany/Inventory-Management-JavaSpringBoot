d4c9d491089e9a156730d50d8b15a4ab5389f57a
Huaqin <53359929+Uibany@users.noreply.github.com>
2020-12-13 19:34:24 +0800

    some fixes

--- team8ca/src/main/java/sg/edu/iss/team8ca/repo/UsageDetailsRepo.java
+++ team8ca/src/main/java/sg/edu/iss/team8ca/repo/UsageDetailsRepo.java
@@ -14,7 +14,7 @@ public interface UsageDetailsRepo extends JpaRepository<UsageDetails, Long> {
 	@Query("Select ud from UsageDetails ud where ud.id = :id")
 	public List<UsageDetails> findUdById(@Param("id") Long id);
 	
-	@Query("Select ud from UsageDetails ud where ud.inventory.id = :id AND (ud.invUsage.date BETWEEN :startDate AND :endDate)")
+	@Query("Select ud from UsageDetails ud where ud.inventory.id = :id AND (ud.invUsage.usageDate BETWEEN :startDate AND :endDate)")
 	public List<UsageDetails> listUsageForInvId(@Param("id")Long id, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 	
 }
