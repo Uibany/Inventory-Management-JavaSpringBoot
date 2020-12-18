15617ddf19fcf80660b3b9d4dc4fa0f4b11470bb
Huaqin <53359929+Uibany@users.noreply.github.com>
2020-12-18 16:00:07 +0800

    fixed stuff

--- team8ca/src/main/java/sg/edu/iss/team8ca/controller/ProductListingController.java
+++ team8ca/src/main/java/sg/edu/iss/team8ca/controller/ProductListingController.java
@@ -27,6 +27,7 @@ import sg.edu.iss.team8ca.model.TransHistory;
 import sg.edu.iss.team8ca.model.TransType;
 import sg.edu.iss.team8ca.model.User;
 import sg.edu.iss.team8ca.service.ProductListingImpl;
+import sg.edu.iss.team8ca.service.SupplierService;
 import sg.edu.iss.team8ca.service.TransHistoryImpl;
 import sg.edu.iss.team8ca.service.UserService;
 
@@ -42,6 +43,9 @@ public class ProductListingController {
 	
 	@Autowired
 	private TransHistoryImpl thservice;
+	
+	@Autowired
+	private SupplierService spservice;
 
 	@Autowired
 	public void setProductListing(ProductListingImpl inventory) {
@@ -119,7 +123,7 @@ public class ProductListingController {
 //		User user = uservice.findUserByUserName(currentUserName);
 //		InvUsage invUsage = new InvUsage(LocalDate.now(), UsageReportStatus.InProgress, user1);
 		User user1 = uservice.findUserByUserName("admin");
-		TransHistory trans = new TransHistory(TransType.NewInventory, Math.toIntExact(inventory.getStockQty()), inventory, LocalDate.now(), LocalTime.now(ZoneId.of("Asia/Tokyo")), user1);
+		TransHistory trans = new TransHistory(TransType.UpdateInventory, Math.toIntExact(inventory.getStockQty()), inventory, LocalDate.now(), LocalTime.now(ZoneId.of("Asia/Tokyo")), user1);
 		thservice.saveTrans(trans);
 		return "redirect:/inventory/list";
 	}
@@ -136,7 +140,7 @@ public class ProductListingController {
 	@RequestMapping(value = "/addbrand")
 	public String addBrand(Model model) {
 		Brand brand = new Brand();
-		ArrayList<String> suplist = plService.findAllSupplierNames();
+		ArrayList<String> suplist = spservice.findAllSupplierNames();
 		model.addAttribute("brand", brand);
 		model.addAttribute("supnames", suplist);
 		return "add-brand";
@@ -144,7 +148,7 @@ public class ProductListingController {
 	
 	@RequestMapping(value = "/savebrand")
 	public String saveBrand(@ModelAttribute("brand") Brand brand, Model model) {
-		Supplier supplier = plService.findSupplierByName(brand.getSupplier().getCompanyName());
+		Supplier supplier = spservice.findSupplierByName(brand.getSupplier().getCompanyName());
 		brand.setSupplier(supplier);
 		plService.addBrand(brand);
 		return "forward:/inventory/addproduct"; 
