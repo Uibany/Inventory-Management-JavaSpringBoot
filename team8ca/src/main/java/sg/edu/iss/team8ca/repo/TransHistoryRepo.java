ab765e993c689030db23f4c970b067217a296c72
Huaqin <53359929+Uibany@users.noreply.github.com>
2020-12-20 20:11:06 +0800

    fixed edit-inventory; more stylings;

--- team8ca/src/main/java/sg/edu/iss/team8ca/repo/TransHistoryRepo.java
+++ team8ca/src/main/java/sg/edu/iss/team8ca/repo/TransHistoryRepo.java
@@ -11,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 import sg.edu.iss.team8ca.model.TransHistory;
 
 public interface TransHistoryRepo extends JpaRepository<TransHistory, Long> {
-	@Query("Select th from TransHistory th where th.inventory.id = :id AND (th.transDate BETWEEN :startDate AND :endDate) ORDER BY th.transDate DESC")
+	@Query("Select th from TransHistory th where th.inventory.id = :id AND (th.transDate BETWEEN :startDate AND :endDate) ORDER BY th.transDate ASC")
 	public List<TransHistory> listTransHisForInvId(@Param("id")long id, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 	
 	@Query("Select th from TransHistory th where th.inventory.id = :id")
