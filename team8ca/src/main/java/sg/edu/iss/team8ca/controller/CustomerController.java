d2200166048c3ffa26e29f4f22c795e54a2bebff
Sean Ooi <69132873+seano188@users.noreply.github.com>
2020-12-21 14:38:00 +0800

    pagination for customer

--- team8ca/src/main/java/sg/edu/iss/team8ca/controller/CustomerController.java
+++ team8ca/src/main/java/sg/edu/iss/team8ca/controller/CustomerController.java
@@ -24,17 +24,17 @@ import sg.edu.iss.team8ca.service.CustomerInterface;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	
+
 	@Autowired
 	private CustomerInterface cuservice;
-	
+
 	@Autowired
 	public void setCustomerService(CustomerImpl customerImpl) {
-		this.cuservice = customerImpl;	
+		this.cuservice = customerImpl;
 	}
-	
-	@RequestMapping(value="/list")
-	public String list (Model model) {
+
+	@RequestMapping(value = "/list")
+	public String list(Model model) {
 		int pageSize = 5;
 		int pageNo = 1;
 		String sortField = "id";
@@ -43,54 +43,63 @@ public class CustomerController {
 		List<Customer> cusList = page.getContent();
 		model.addAttribute("cusList", cusList);
 		model.addAttribute("currentPage", pageNo);
+		model.addAttribute("pageSize", pageSize);
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("totalItems", page.getTotalElements());
 		model.addAttribute("sortField", sortField);
 		model.addAttribute("sortDir", sortDirection);
+		model.addAttribute("reverseSortDir", sortDirection.equals("asc") ? "desc" : "asc");
 		return "CustomerListing";
 	}
-	
+
 	@RequestMapping(value = "/search/page/{pageNo}/{pageSize}", method = RequestMethod.GET)
-	public String searchWithPage(String keyword ,@PathVariable ( value = "pageNo") int pageNo, 
-			@PathVariable ( value = "pageSize") int pageSize, 
-			@RequestParam ("sortField") String sortField,
-			@RequestParam ("sortDir")String sortDir, Model model)  {
-		
-		Page<Customer> page = cuservice.cusSearchPage(keyword, pageNo, pageSize, sortField, sortDir);
-		List<Customer> cusList = page.getContent();
-		
-		model.addAttribute("cusList", cusList);
-		model.addAttribute("currentPage", pageNo);
-		model.addAttribute("totalPages", page.getTotalPages());
-		model.addAttribute("totalItems", page.getTotalElements());
-		return "product-listing";
+	public String searchWithPage(String keyword, @PathVariable(value = "pageNo") int pageNo,
+			@PathVariable(value = "pageSize") int pageSize, @RequestParam("sortField") String sortField,
+			@RequestParam("sortDir") String sortDirection, Model model) {
+		if (keyword == null) {
+			return "forward:/customer/list";
+		} else {
+			Page<Customer> page = cuservice.cusSearchPage(keyword, pageNo, pageSize, sortField, sortDirection);
+			List<Customer> cusList = page.getContent();
+
+			model.addAttribute("cusList", cusList);
+			model.addAttribute("currentPage", pageNo);
+			model.addAttribute("pageSize", pageSize);
+			model.addAttribute("totalPages", page.getTotalPages());
+			model.addAttribute("totalItems", page.getTotalElements());
+			model.addAttribute("keyword", keyword);
+			model.addAttribute("sortField", sortField);
+			model.addAttribute("sortDir", sortDirection);
+			model.addAttribute("reverseSortDir", sortDirection.equals("asc") ? "desc" : "asc");
+			return "CustomerListing";
+		}
 	}
-	
-	@RequestMapping(value="/add")
+
+	@RequestMapping(value = "/add")
 	public String addCustomer(Model model) {
 		model.addAttribute("customer", new Customer());
 		return "CustomerForm";
 	}
-	
+
 	@RequestMapping(value = "/edit/{id}")
 	public String editForm(@PathVariable("id") Long id, Model model) {
 		model.addAttribute("customer", cuservice.findCustomerById(id));
 		return "CustomerForm";
 	}
-	
+
 	@RequestMapping(value = "/save")
-	public String saveCustomer(@ModelAttribute("customer") @Valid Customer customer, BindingResult bindingResult, Model model) {
+	public String saveCustomer(@ModelAttribute("customer") @Valid Customer customer, BindingResult bindingResult,
+			Model model) {
 		if (bindingResult.hasErrors()) {
 			return "CustomerForm";
 		}
 		cuservice.saveCustomer(customer);
 		return "forward:/customer/list";
 	}
-	
+
 	@RequestMapping(value = "/delete/{id}")
 	public String deleteCustomer(@PathVariable("id") Long id) {
 		cuservice.deleteSupplier(cuservice.findCustomerById(id));
 		return "forward:/customer/list";
-	}	
+	}
 }
-	
--- team8ca/src/main/java/sg/edu/iss/team8ca/controller/ProductListingController.java
+++ team8ca/src/main/java/sg/edu/iss/team8ca/controller/ProductListingController.java
@@ -259,7 +259,7 @@ public class ProductListingController {
 	}
 	@RequestMapping("/report")
 	public String reorderReport() {
-		reorser.printDatFile();
+		reorser.printDatFile(1);
 		return "redirect:/inventory/list";
 	}
 	
