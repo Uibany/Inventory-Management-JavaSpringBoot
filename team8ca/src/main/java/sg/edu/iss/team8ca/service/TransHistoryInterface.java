07df481a793560cf56b92e0f039e824e7235eeab
Huaqin <53359929+Uibany@users.noreply.github.com>
2020-12-22 11:49:34 +0800

    transaction type filter and other stuff

--- team8ca/src/main/java/sg/edu/iss/team8ca/service/TransHistoryInterface.java
+++ team8ca/src/main/java/sg/edu/iss/team8ca/service/TransHistoryInterface.java
@@ -10,6 +10,10 @@ public interface TransHistoryInterface {
 	public List<TransHistory> listAllTransHis();
 	public List<TransHistory> listTransHisForId(long id);
 	public List<TransHistory> listTransHisForDate(long id, LocalDate startDate, LocalDate endDate);
+	
+	public List<TransHistory> listTransHisForIdType(long id, String transType);
+	public List<TransHistory> listTransHisForDateType(long id, LocalDate startDate, LocalDate endDate, String transType);
+	
 	public Inventory findInvById(long id);
 	public void saveTrans(TransHistory transHistory);
 	
